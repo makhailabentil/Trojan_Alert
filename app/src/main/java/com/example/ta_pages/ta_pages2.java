0962a7a724e373264fe2c7a838784cde2a8856b7
benti <bentil.makhaila@gmail.com>
2023-11-10 12:40:22 -0500

    Commit the after VSU 2023 Hackathon

---
+++ app/src/main/java/com/example/ta_pages/ta_pages2.java
@@ -0,0 +1,14 @@
+package com.example.ta_pages;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ta_pages2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ta_pages2);
+    }
+}
